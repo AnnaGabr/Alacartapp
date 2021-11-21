0dec1fdec840f1b40531071999f6e7a8769a8f03
Anna <annagabriela2940@gmail.com>
2021-11-21 13:14:47 -0500

    Se agregan nuevas carpetas

--- app/src/main/java/com/example/alacartapp/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/alacartapp/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.alacartapp
+package com.example.alacartapp.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.alacartapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
