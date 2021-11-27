cc47803c1826ad8c00e7906aba41d565ef4cf312
Anna <annagabriela2940@gmail.com>
2021-11-27 15:13:19 -0500

    Se conecto MainActivity con el fragmento Menu para que lo mostrara al inicio

--- app/src/main/java/com/example/alacartapp/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/alacartapp/view/ui/activities/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.alacartapp.view.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.alacartapp.R
+import com.example.alacartapp.view.ui.fragments.MenuFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, MenuFragment())
+                .commit()
+        }
     }
 }
