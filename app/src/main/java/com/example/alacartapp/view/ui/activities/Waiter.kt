1ec016a3823f0ab36683bb5b6edaba5c6fd5212e
frecriva <frecriva@gmail.com>
2021-12-01 11:53:59 -0500

    incorpocacion de cambios, fragment_client

--- app/src/main/java/com/example/alacartapp/view/ui/activities/Waiter.kt
+++ app/src/main/java/com/example/alacartapp/view/ui/activities/Waiter.kt
@@ -3,18 +3,19 @@ package com.example.alacartapp.view.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.alacartapp.R
-import com.example.alacartapp.view.ui.fragments.MenuFragment
+import com.example.alacartapp.view.ui.fragments.Waiter_login
 
-class MainActivity : AppCompatActivity() {
+class Waiter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_waiter)
 
         if(savedInstanceState == null){
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.container, MenuFragment())
+                .add(R.id.container, Waiter_login())
                 .commit()
         }
+
     }
 }
